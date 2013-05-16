221bdbfe0dcf0413653e6719501c357d5e45e7a0
Josiah Gore <josiahgore@gmail.com>
2013-05-16 14:30:58 -0400

    fix hgignore and add stub for class that was missed as a result

---
+++ src/main/java/com/subrosagames/subrosa/mobile/android/target/ViewTargetActivity.java
@@ -0,0 +1,11 @@
+package com.subrosagames.subrosa.mobile.android.target;
+
+import android.os.AsyncTask;
+
+public class ViewTargetActivity extends AsyncTask<Void, Void, String> {
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        return null;
+    }
+}
